abd214930ea84ffeab84e6f19d740affa151b00a
Pengpa0012 <peng.ilikha@gmail.com>
2023-11-07 23:08:16 +0800

    added initial todo routes

--- main.go
+++ main.go
@@ -5,10 +5,26 @@ import (
     "github.com/labstack/echo/v4"
 )
 
+func getTodos(c echo.Context) {
+    c.String(http.StatusOK, "Get Todos")
+}
+
+func addTodo(c echo.Context) {
+    c.String(http.StatusOK, "Add Todo")
+}
+
+func removeTodo(c echo.Context) {
+    c.String(http.StatusOK, "Remove Todo")
+}
+
+func updateTodo(c echo.Context) {
+    c.String(http.StatusOK, "Update Todo")
+}
+
 func main() {
     e := echo.New()
     e.GET("/", func(c echo.Context) error {
         return c.String(http.StatusOK, "Hello, World!")
     })
-    e.Logger.Fatal(e.Start(":1323"))
+    e.Logger.Fatal(e.Start(":9090"))
 }
