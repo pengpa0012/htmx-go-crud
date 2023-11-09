43780358fba61cf91cd142e7912aff32ded9e528
Pengpa0012 <peng.ilikha@gmail.com>
2023-11-09 08:01:21 +0800

    added addTodo

--- main.go
+++ main.go
@@ -4,36 +4,52 @@ import (
     "net/http"
     "github.com/labstack/echo/v4"
     "github.com/labstack/echo/v4/middleware"
+    "math/rand"
+    "time"
 )
 
 type Todo struct {
-	ID     string  `json:"id"`
+	ID     int  `json:"id"`
 	Title  string  `json:"title"`
 	Description  string  `json:"description"`
-	DateCreated  int  `json:"date_created"`
+	DateCreated  time.Time  `json:"date_created"`
 	Completed  bool `json:"completed"`
 }
 
 var todos = []Todo {
-    {ID: "1", Title: "Work", Description: "Do the thing", DateCreated: 1699404506241, Completed: false},
-	{ID: "2", Title: "Personal Time", Description: "Go to gym", DateCreated: 1699404506241, Completed: false},
-	{ID: "3", Title: "Fun Time", Description: "Play games", DateCreated: 1699404506241, Completed: false},
+    {ID: 1, Title: "Work", Description: "Do the thing", DateCreated: time.Now(), Completed: false},
+	{ID: 2, Title: "Personal Time", Description: "Go to gym", DateCreated: time.Now(), Completed: false},
+	{ID: 3, Title: "Fun Time", Description: "Play games", DateCreated: time.Now(), Completed: false},
 }
 
 func getTodos(c echo.Context) error {
-    return c.HTML(http.StatusOK, "<h1>Add Todo</h1>")
+    return c.JSON(http.StatusOK, todos)
 }
 
 func addTodo(c echo.Context) error {
-    return c.String(http.StatusOK, "Add Todo")
+    rand.Seed(time.Now().UnixNano())
+    title := c.FormValue("title")
+    description := c.FormValue("description")
+    todo := Todo {
+        ID: rand.Intn(1000), 
+        Title: title,
+        Description: description,
+        DateCreated: time.Now(),
+    }
+
+    todos = append(todos, todo)
+
+    return c.JSON(http.StatusOK, todos)
 }
 
 func removeTodo(c echo.Context) error {
-    return c.String(http.StatusOK, "Remove Todo")
+    id := c.QueryParam("id")
+    return c.JSON(http.StatusOK, id)
 }
 
 func updateTodo(c echo.Context) error {
-    return c.String(http.StatusOK, "Update Todo")
+    id := c.QueryParam("id")
+    return c.JSON(http.StatusOK, id)
 }
 
 func main() {
